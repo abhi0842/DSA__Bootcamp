5f17a4e6bf5a99301677f73a509fdf018acec746
Abhi <007abhinavchaudhary@gmail.com>
2026-02-23 00:38:28 +0530

    bitdistance

--- src/Bitwise/bitdistance.java
+++ src/Bitwise/bitdistance.java
@@ -1,23 +1,36 @@
 package Bitwise;
 
 public class bitdistance {
-    public  static int binaryGap(int n) {
-        int pos =0;
+//    public  static int binaryGap(int n) {
+//        int pos =0;
+//        int last=-1;
+//        int dist=0;
+//        while(n>0){
+//            if((n&1)==1){
+//                if (last != -1) {
+//                    dist = Math.max(dist, pos - last);
+//                }
+//                last = pos;
+//            }
+//            pos++;
+//            n >>= 1;
+//        }
+//        return dist;
+//    }
+    public static int binaryGap(int n) {
+        String s = Integer.toBinaryString(n);
         int last=-1;
         int dist=0;
-        while(n>0){
-            if((n&1)==1){
-                if (last != -1) {
-                    dist = Math.max(dist, pos - last);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                if(last!=-1){
+                    dist=Math.max(dist,i-last);
                 }
-                last = pos;
+                last = i;
             }
-            pos++;
-            n >>= 1;
         }
         return dist;
     }
-
     public static void main(String[] args) {
         int n=22;
         System.out.println(binaryGap(n));
